11dae4883192dce9aa0b17e81ee142fcca7935da
velsenthilkumar1988 <velsenthilkumar1988@gmail.com>
2024-02-16 23:09:53 +0530

    update successful random color

--- src/Components/Random-Color/index.jsx
+++ src/Components/Random-Color/index.jsx
@@ -23,7 +23,7 @@ export default function RandomColor(){
         const r = randomColorUtility(256);
         const g = randomColorUtility(256);
         const b = randomColorUtility(256);
-        setColor(`rgb${r},${g},${b}`);
+        setColor(`rgb(${r},${g},${b})`);
     }
 
 
