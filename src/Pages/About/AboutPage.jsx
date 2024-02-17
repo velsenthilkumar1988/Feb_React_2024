6981f2c52b730258fdeaa498d8a0faf156177bd3
velsenthilkumar1988 <velsenthilkumar1988@gmail.com>
2024-02-17 19:26:25 +0530

    star rating

--- src/Pages/About/AboutPage.jsx
+++ src/Pages/About/AboutPage.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Accordion from '../../Components/Accordion'
 import RandomColor from '../../Components/Random-Color'
+import StarRating from '../../Components/Star-Rating'
 
 const AboutPage = () => {
   return (
     <div>
       <Accordion/>
       <RandomColor/>
+      <StarRating noOfStars={10}/>
     </div>
   )
 }
